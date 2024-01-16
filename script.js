d983f348bf128803c17dbca7b1a5270dc0152699
TienNguyen99 <cuongem1008@gmail.com>
2024-01-16 13:02:56 +0700

    completed

--- script.js
+++ script.js
@@ -61,3 +61,25 @@ sr.reveal('.skills__data, .work__img, .contact__input',{interval: 200});
           loop:true,
           backSpeed: 50
         });
+
+// // Modal
+// // Get the modal
+// var modal = document.getElementById("myModal");
+
+// // Get the image and insert it inside the modal - use its "alt" text as a caption
+// var img = document.getElementById("project1");
+// var modalImg = document.getElementById("img01");
+// var captionText = document.getElementById("caption");
+// img.onclick = function(){
+//   modal.style.display = "block";
+//   modalImg.src = this.src;
+//   captionText.innerHTML = this.alt;
+// }
+
+// // Get the <span> element that closes the modal
+// var span = document.getElementsByClassName("close")[0];
+
+// // When the user clicks on <span> (x), close the modal
+// span.onclick = function() {
+//   modal.style.display = "none";
+// }
